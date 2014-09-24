c665601bd4bb58ff6284a55b67e1be895da24a1f
Matt <mjs7769@rit.edu>
2014-09-24 13:42:29 -0400

    Add scaffolding

---
+++ Philosopher.java
@@ -0,0 +1,9 @@
+public class Philosopher extends Thread {
+	public Philosopher(int id, Fork left, Fork right, boolean rHanded, int nTimes, long thiknMillis, long eatMillis) {
+
+	}
+
+	public void run() {
+
+	}
+}
