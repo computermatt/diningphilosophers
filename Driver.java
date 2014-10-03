f14f2950d1db5f877e70c44af13918c3a4805aac
David Wilson <dmw9070@rit.edu>
2014-10-03 09:06:08 -0400

    Allowing argument omission
    checked arg length for each parameter rather than as a whole, so args
    can be omitted to be left as default.

--- Driver.java
+++ Driver.java
@@ -1,39 +1,38 @@
 public class Driver {
 	public static void main(String[] args) {
-		//not necessary to initialize them with defaults, but w/e 
 		int np = 4;
 		int nt = 10;
 		long tm = 0;
 		long em = 0;
 		boolean handedness = false;
 
-		if (args.length > 3) {
-			int startingArg = 0;
-			if (args[0].equals("-l")) {
-				handedness = true;
-				startingArg = 1;
-			}
+		int startingArg = 0;
+		if (args.length > startingArg && args[startingArg].equals("-l")) {
+			handedness = true;
+			startingArg++;
+		}
+		
+		if (args.length > startingArg)
 			np = Integer.parseInt(args[startingArg++]);
+		if (args.length > startingArg)
 			nt = Integer.parseInt(args[startingArg++]);
+		if (args.length > startingArg)
 			tm = Long.parseLong(args[startingArg++]);
+		if (args.length > startingArg)
 			em = Long.parseLong(args[startingArg]);
 
-			Fork[] forks = new Fork[np];
-			for(int i=0; i < np; i++) {
-				forks[i] = new Fork();
-			}
+		Fork[] forks = new Fork[np];
+		for(int i=0; i < np; i++) {
+			forks[i] = new Fork();
+		}
 
-			for(int i=0; i < np; i++) {
-				boolean rightHanded = true;
-				if(handedness && i % 2 == 1) {
-					rightHanded = false;
-				}
-				Philosopher philo = new Philosopher(i, forks[i], forks[(np + i - 1) % np], rightHanded, nt, tm, em);
-				philo.start();
+		for(int i=0; i < np; i++) {
+			boolean rightHanded = true;
+			if(handedness && i % 2 == 1) {
+				rightHanded = false;
 			}
-
-		} else {
-			System.out.println("Invalid args.");
+			Philosopher philo = new Philosopher(i, forks[i], forks[(np + i - 1) % np], rightHanded, nt, tm, em);
+			philo.start();
 		}
 	}
 
